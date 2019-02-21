bfafa5930d77da2db307b224caa94313a3560230
strtrek <geyuanji@strtrek.com>
2019-02-21 17:55:29 +0800

    增加多进程处理

--- StatisticalAnalysis.py
+++ StatisticalAnalysis.py
@@ -21,10 +21,9 @@ BlockSize = 4096                    # 块行数
 Batches = 100                       # 批量
 ChunkSize = BlockSize * Batches     # 批行数
 Accuracy = 10000000000              # 精度
-Processes = 5                       # 进程数
-BlockingTime = 2                    # 堵塞时间 = 子进程处理时间
-Threads = 5                         # 线程数
-Debugging = 10                                  # 调试轮询
+Processes = 8                       # 进程数
+BlockingTime = 0.025                # 堵塞时间 = 子进程处理时间 / Processes
+Debugging = 20                                  # 调试轮询
 pd.set_option('display.max_columns', 10)        # 调试列数
 pd.set_option('display.max_rows', 500)          # 调试行数
 pd.set_option('display.width', 2000)            # 调试宽度
@@ -34,58 +33,65 @@ def print_run_time(func):
     def wrapper(*args, **kw):
         local_time = time.time()
         func(*args, **kw)
-        print('Function [%s] run %.2f' % (func.__name__, time.time() - local_time))
+        print('Function [%s] run %.4f seconds.' % (func.__name__, time.time() - local_time))
     return wrapper
 
 
-def get_sequence(elem):
+def get_indicator(elem):
     return elem[0]
 
 
-def remove_sequence(elems):
-    without_seq = []
+def remove_indicator(elems):
+    without_indicator = []
     for elem in elems:
-        without_seq.append(elem[1])
-    return without_seq
+        without_indicator.append(elem[1])
+    return without_indicator
 
 
-def product_collect(queue:Queue, df_stat:pd.DataFrame):
-    return
+def product_collect(queue_products: Queue, df_stat: pd.DataFrame, cnt_products: int):
+    products = [(0, df_stat)]
+    while not queue_products.empty():
+        indicator, chunk_stat = queue_products.get()
+        products.append((indicator, pd.DataFrame(chunk_stat)))
+        cnt_products = cnt_products + 1
+    if len(products) > 1:
+        products.sort(key=get_indicator)
+        products = remove_indicator(products)
+        df_stat = pd.concat(products, ignore_index=True)
+    return cnt_products, df_stat
+
+
+def material_prepare(queue_materials: Queue, args: tuple):
+    chunk, indicator, subset = args
+    while queue_materials.full():
+        time.sleep(BlockingTime)
+    queue_materials.put((chunk, indicator, subset))
 
 
 def data_statistical(path, subset: str):
-
-
     data = pd.read_csv(path, iterator=True, dtype=str)
     loop = True
     indicator = 0
     df_stat = pd.DataFrame(columns=["start", "stop", "begin", "end", "min", "max", "mean"])
-    process_pool = Pool(Processes)              # 进程池
-    queue_material = Manager().Queue(Processes)      # 待加工队列
-    queue_product = Manager().Queue(Processes)             # 输出队列
+    process_pool = Pool(Processes)                   # 进程池
+    queue_materials = Manager().Queue(Processes)     # 原料队列
+    cnt_materials = 0                                # 发料计数
+    queue_products = Manager().Queue(Processes)      # 成品队列
+    cnt_products = 0                                 # 出厂计数
     while loop:
         try:
             chunk = data.get_chunk(ChunkSize)
-            # Setting accuracy
-            chunk[subset] = chunk.apply(lambda x: int(float(x[subset]) * Accuracy), axis=1)
+            chunk_length = len(chunk)
             # Material prepare
-            while queue_material.full():
+            while queue_materials.full():
                 time.sleep(BlockingTime)
-            queue_material.put((chunk, indicator, subset))
-            process_pool.apply_async(func=chunk_statistical, args=(queue_material, queue_product))      # 非堵塞调度
+            queue_materials.put((chunk, indicator, subset))
+            process_pool.apply_async(func=chunk_statistical, args=(queue_materials, queue_products))      # 非堵塞异步
+            cnt_materials = cnt_materials + 1       # 发料计数
             # Product collect
-            product = []
-            while not queue_product.empty():
-                i, chunk_stat = queue_product.get()
-                product.append((i, pd.DataFrame(chunk_stat)))
-            product.sort(key=get_sequence)
-            product = remove_sequence(product)
-            if len(product) > 0:
-                df_stat = pd.concat([df_stat, ].extend(product), ignore_index=True)
-            else:
-                pass
+            cnt_products, df_stat = product_collect(queue_products, df_stat, cnt_products)
             # Indicator
-            indicator = indicator + len(chunk)
+            indicator = indicator + chunk_length
             # Debugging
             if (indicator > (ChunkSize * Debugging)) and (Debugging > 0):
                 loop = False
@@ -93,16 +99,9 @@ def data_statistical(path, subset: str):
         except StopIteration:
             loop = False
             print("Iteration is stopped at {0}.".format(indicator))
-            # Product collect
-            product = []
-            while not queue_material.empty():
-                time.sleep(BlockingTime)
-            while not queue_product.empty():
-                i, chunk_stat = queue_product.get()
-                product.append((i, pd.DataFrame(chunk_stat)))
-            product.sort(key=get_sequence)
-            product = remove_sequence(product)
-            df_stat = pd.concat(product, ignore_index=True)
+    # Product collect
+    while cnt_products < cnt_materials:
+        cnt_products, df_stat = product_collect(queue_products, df_stat, cnt_products)
     process_pool.close()
     process_pool.join()
     return {
@@ -122,7 +121,11 @@ def block_statistical(block: pd.DataFrame, subset: str):
 
 
 def chunk_statistical(queue_in: Queue, queue_out: Queue):
+    start_time = time.time()
     chunk, indicator, subset = queue_in.get()
+    # Setting accuracy
+    chunk[subset] = chunk.apply(lambda x: int(float(x[subset]) * Accuracy), axis=1)
+    # Statistical table
     chunk_stat = {
         "start": [],
         "stop": [],
@@ -143,9 +146,8 @@ def chunk_statistical(queue_in: Queue, queue_out: Queue):
         chunk_stat["min"].append(block_stat["min"])
         chunk_stat["max"].append(block_stat["max"])
         chunk_stat["mean"].append(block_stat["mean"])
-    print("STAT at {0}-{1}".format(indicator, indicator + ChunkSize))
+    print("STAT {0}-{1} @ {2} seconds.".format(indicator, indicator + ChunkSize, time.time() - start_time))
     queue_out.put((indicator, chunk_stat))
-    # return chunk_stat
 
 
 @print_run_time
@@ -153,7 +155,7 @@ def main():
     train_file_path = "data/train.csv"
     dt = data_statistical(train_file_path, "time_to_failure")
     df = dt["DataFrame"]
-    # print(df)
+    print(df)
     # df.to_excel("data/statistical.xlsx")
 
 
